73642e246d541be403530a95ea242ef2f00cf7bb
Rakshithabk <rakshithabk16@gmail.com>
2020-08-12 20:23:07 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def base(request):
+    return render(request,"base.html")
+
+
+def multi(request):
+    if request.method=="POST":
+        foods=request.POST.getlist("food")
+        languages=request.POST.getlist("language")
+        return HttpResponse("<h1>{}{}<h1>".format(foods,languages))
+    return render(request,'multiselect.html')
+
+from django.core.files.storage import FileSystemStorage
+
+def img_upld(request):
+    if request.method=="POST" and request.FILES:
+        image=request.FILES['sam']
+        fs=FileSystemStorage()
+        fs.save(image.name,image)
+                
+    return render(request,"img_upld.html")
